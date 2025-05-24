6124df6ddf971084e24ea768066e0da86897c76a
Sebastian <sebastianmcconnell01@gmail.com>
2025-05-24 18:37:54 +0200

    fixed routes.py

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -9,7 +9,7 @@ def show_movies():
     with db.cursor() as cur:
         cur.execute("SELECT movie_id, title, year, director, genre, run_time, IMDB_rating, num_votes FROM movie")
         movies = cur.fetchall()
-        column_names = [desc[0] for desc in cur.description]
+        column_names = [desc[0] for desc in cur.description] if cur.description else []
     return render_template('movies.html', movies=movies, column_names=column_names)
 
 @bp.route('/actors')
@@ -18,7 +18,7 @@ def show_actors():
     with db.cursor() as cur:
         cur.execute("SELECT actor_id, actor_name FROM actors")
         actors = cur.fetchall()
-        column_names = [desc[0] for desc in cur.description]
+        column_names = [desc[0] for desc in cur.description] if cur.description else []
     return render_template('actors.html', actors=actors, column_names=column_names)
 
 @bp.route('/watchlist')
@@ -31,23 +31,23 @@ def show_watchlist():
         if not movie_ids:
             movies = []
             column_names = []
-        else:
-            format_strings = ','.join(['%s'] * len(movie_ids))
-            cur.execute(f"""
+        # format_strings = ','.join(['%s'] * len(movie_ids))
+        movies = []
+        for movie_id in movie_ids:
+            cur.execute("""
                 SELECT movie_id, title, year, director, genre, run_time, IMDB_rating, num_votes
                 FROM movie
-                WHERE movie_id IN ({format_strings})
-            """, tuple(movie_ids))
-            movies = cur.fetchall()
-            column_names = [desc[0] for desc in cur.description]
+                WHERE movie_id = %s
+            """, (movie_id,))
+            movies += cur.fetchall()
+        column_names = [desc[0] for desc in cur.description] if cur.description else []
     return render_template('watchlist_movies.html', movies=movies, column_names=column_names)
 
 
 
 @bp.route('/add_rating', methods=['POST'])
 def add_rating():
-    #user_id = session["UserName"]
-    user_id = "sebastian"
+    user_id = session["username"]
     movie_id = request.form['movie_id']
     rating = request.form['rating']
 
@@ -61,21 +61,23 @@ def add_rating():
             "SELECT IMDB_rating, num_votes FROM movie WHERE movie_id = %s",
             (movie_id,)
         )
-        imdb_rating, num_votes = cur.fetchone()
-        new_num_votes = num_votes + 1
-        new_imdb_rating = (imdb_rating * num_votes + float(rating)) / (new_num_votes)
-        cur.execute(
-            "UPDATE movie SET IMDB_rating = %s, num_votes = %s WHERE movie_id = %s",
-            (new_imdb_rating, new_num_votes, movie_id)
-        )
-        db.commit()
+        result = cur.fetchone()
+        if result:
+            imdb_rating, num_votes = result
+            new_num_votes = num_votes + 1
+            new_imdb_rating = (imdb_rating * num_votes + float(rating)) / (new_num_votes)
+            cur.execute(
+                "UPDATE movie SET IMDB_rating = %s, num_votes = %s WHERE movie_id = %s",
+                (new_imdb_rating, new_num_votes, movie_id)
+            )
+            db.commit()
 
     return redirect(url_for(''))
 
 @bp.route('/change_rating', methods=['POST'])
 def change_rating():
     #user_id = session["UserName"]
-    user_id = "sebastian"
+    user_id = session["username"]
     movie_id = request.form['movie_id']
     rating = request.form['rating']
 
@@ -89,20 +91,21 @@ def change_rating():
             "SELECT IMDB_rating, num_votes FROM movie WHERE movie_id = %s",
             (movie_id,)
         )
-        imdb_rating, num_votes = cur.fetchone()
-        new_imdb_rating = (imdb_rating * num_votes - float(rating)) / (num_votes)
-        cur.execute(
-            "UPDATE movie SET IMDB_rating = %s WHERE movie_id = %s",
-            (new_imdb_rating, movie_id)
-        )
-        db.commit()
+        result = cur.fetchone()
+        if result:
+            imdb_rating, num_votes = result
+            new_imdb_rating = (imdb_rating * num_votes - float(rating)) / (num_votes)
+            cur.execute(
+                "UPDATE movie SET IMDB_rating = %s WHERE movie_id = %s",
+                (new_imdb_rating, movie_id)
+            )
+            db.commit()
 
     return redirect(url_for(''))
 
 @bp.route('/delete_rating', methods=['POST'])
 def delete_rating():
-    #user_id = session["UserName"]
-    user_id = "sebastian"
+    user_id = session["username"]
     movie_id = request.form['movie_id']
 
     db = get_db()
@@ -115,14 +118,17 @@ def delete_rating():
             "SELECT IMDB_rating, num_votes FROM movie WHERE movie_id = %s",
             (movie_id,)
         )
-        imdb_rating, num_votes = cur.fetchone()
-        new_num_votes = num_votes - 1
-        new_imdb_rating = (imdb_rating * num_votes) / (new_num_votes)
-        cur.execute(
-            "UPDATE movie SET IMDB_rating = %s, num_votes = %s WHERE movie_id = %s",
-            (new_imdb_rating, new_num_votes, movie_id)
-        )
-        db.commit()
+        result = cur.fetchone()
+        
+        if result:
+            imdb_rating, num_votes = result
+            new_num_votes = num_votes - 1
+            new_imdb_rating = (imdb_rating * num_votes) / (new_num_votes)
+            cur.execute(
+                "UPDATE movie SET IMDB_rating = %s, num_votes = %s WHERE movie_id = %s",
+                (new_imdb_rating, new_num_votes, movie_id)
+            )
+            db.commit()
 
     return redirect(url_for(''))
 
@@ -130,12 +136,12 @@ def delete_rating():
 @bp.route('/rating')
 def show_rating():
     #user_id = session["UserName"]
-    user_id = "sebastian"
+    user_id = session['username']
     db = get_db()
     with db.cursor() as cur:
         cur.execute("SELECT movie_id, rating FROM rating WHERE user_id = %s", (user_id,))
         rating = cur.fetchall()
-        column_names = [desc[0] for desc in cur.description]
+        column_names = [desc[0] for desc in cur.description] if cur.description else []
     return render_template('')
 
 

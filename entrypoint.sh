21f3e41e6ace81713467aae8e1bd49417b2dd2ef
Sebastian <sebastianmcconnell01@gmail.com>
2025-06-02 15:52:56 +0200

    DB initialized once

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "Waiting for database..."
-sleep 5
+# echo "Waiting for database..."
+# sleep 5
 
 # Run database migrations
-flask init-db
-sleep 5
+# flask init-db
+# sleep 5
 exec flask run --debug --host=0.0.0.0 --port=5001
